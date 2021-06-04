8bf79fea5420e3474d2d6f8ec89bf47a0f88bc61
jie.zeng <1063423131@qq.com>
2021-06-04 20:55:05 +0800

    init

---
+++ src/main/java/com/zengjie/algorithm/search/BinarySearchTest.java
@@ -0,0 +1,12 @@
+package com.zengjie.algorithm.search;
+
+/**
+ * @author jie.zeng
+ * @version 1.0
+ * @since 2021/6/4 20:50
+ */
+public class BinarySearchTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zengjie/algorithm/tree/NormalTreeTest.java
@@ -0,0 +1,12 @@
+package com.zengjie.algorithm.tree;
+
+/**
+ * @author jie.zeng
+ * @version 1.0
+ * @since 2021/6/4 20:54
+ */
+public class NormalTreeTest {
+    public static void main(String[] args) {
+
+    }
+}
